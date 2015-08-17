02ef66a77c37b932042b84913ec4c80b9832af98
Robin <Robin@Y50-PC>
2015-08-17 12:05:54 +0200

    Änderungen an Material SAP

--- src/SAPumbau/MaterialSAP.java
+++ src/SAPumbau/MaterialSAP.java
@@ -1,5 +1,11 @@
 package SAPumbau;
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import javax.swing.JTable;
+
+import com.sap.conn.jco.JCo;
 import com.sap.conn.jco.JCoContext;
 import com.sap.conn.jco.JCoDestination;
 import com.sap.conn.jco.JCoDestinationManager;
@@ -7,6 +13,7 @@ import com.sap.conn.jco.JCoException;
 import com.sap.conn.jco.JCoFunction;
 import com.sap.conn.jco.JCoRepository;
 import com.sap.conn.jco.JCoStructure;
+import com.sap.conn.jco.JCoTable;
 
 
 /**
@@ -15,12 +22,15 @@ import com.sap.conn.jco.JCoStructure;
  */
 public class MaterialSAP {
 
+	private int anzahlMat;
+	private ArrayList<String> Materialliste = new ArrayList<String>();
 	public MaterialSAP() {
 		// TODO Auto-generated constructor stub
 	}
 
 	public void materialListeHolen()
 	{
+		
 		try {
 			//Abfragen ob ein Ziel(Das SAP System vorhanden ist)
 			JCoDestination dest = JCoDestinationManager.getDestination("");
@@ -46,8 +56,24 @@ public class MaterialSAP {
 			JCoContext.end(dest);
 
 			//Rückgabewert engegennehmen (SAP Kundennummer/Debitor)
-			System.out.println(func.getExportParameterList().getValue("RETURN"));
-			//System.out.println(func.getTableParameterList().getTable("RETURN"));
+			
+			
+			//Materialliste holen:
+			Materialliste.clear();
+			JCoTable table = func.getTableParameterList().getTable("ITEMS");
+			anzahlMat = table.getNumRows();
+			for(int i = 0; i<anzahlMat; i++)
+			{
+				table.setRow(i);
+				Materialliste.add(table.getString("MATERIAL"));
+				System.out.println(table.getString("MATERIAL"));
+			}
+			
+			
+			
+			
+			
+			
 			
 		} catch (JCoException e) {
 			// TODO Auto-generated catch block
